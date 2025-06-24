050ced8967f37c038a08729cd70a2c672aaa8a93
socarte <socarte.barreiro@gmail.com>
2025-06-24 14:28:00 -0300

    subi o projeto inicial

---
+++ AULA1/AULA1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace AULA1
+{
+    class Program
+    {
+        static void Variaveis()
+        {
+            /**
+Console.WriteLine("terminal de lucas");
+
+
+//uso para inteiros
+int pontos;
+pontos = 0;
+int vida;
+vida = 3;
+
+float velocidade = 22.5f;  //esse é menor
+double dano;
+
+/*   TIPOS DE TEXTOS   */
+
+            string nome;
+            //criem uma variavel chamada titulo e atribua o nome do seu jogo
+            string titulo = "marte ataca";
+            char letra;
+            /*  TIPOS BOLEANOS  */
+            bool playing = false;
+        }
+    }
+}
---
+++ AULA1/AULA1/estrategit.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+
+namespace AULA1
+{
+    class estrategit
+    {
+        static char[,] mapa;
+        static int largura = 20;
+        static int altura = 10;
+        static int playerX = 1;
+        static int playerY = 1;
+        static bool jogando = true;
+
+        public static void Main()
+        {
+
+            //TODO menu
+            jogar();
+
+        }
+
+        public static void jogar()
+        {
+            while (jogando)
+            {
+                Console.Clear();
+                desenhaMapa();
+
+                var tecla = Console.ReadKey(true).Key;
+
+                atualizarPosicao(tecla);
+
+            }
+        }
+        static void iniciarmapa()
+        {
+            mapa = new char[largura, altura];
+
+            for (int y = 0; y < altura; y++)
+            {
+                for (int x = 0; x < largura; x++)
+                {
+                    if (x == 0 || y == 0 || x == largura - 1 || y == altura - 1)
+                    {
+                        mapa[x, y] = '#';
+                    }
+                    else
+                    {
+                        mapa[x, y] = ' ';
+                    }
+
+                }
+
+            }
+
+            mapa[playerX, playerY] = '@';
+        }
+
+        static void desenhaMapa()
+        {
+            for (int y = 0; y < altura; y++)
+            {
+                for (int x = 0; x < largura; x++)
+                {
+                    Console.Write(mapa[x,y]  );
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void atualizarPosicao(ConsoleKey tecla)
+        {
+            int tempx = playerX;
+            int tempy = playerY;
+
+            switch (tecla) {
+                case ConsoleKey.A:
+                    tempx--;
+                    break;
+                case ConsoleKey.D:
+                    tempx++;
+                    break;
+                case ConsoleKey.W:
+                    tempy--;
+                    break;
+                case ConsoleKey.S:
+                    tempy++;
+                    break;
+            }
+
+            if (mapa[tempx,tempy] != '#') { 
+            }
+
+
+        }
+        
+
+    }
+    
+}
